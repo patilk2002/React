9be67dd879f67b489d6789ffee35dc1c1a5859b7
kiran <kiran.kp2004@gmail.com>
2022-06-27 18:37:29 +0530

    Day2 done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 let name="Kiran";
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container">
-      <h1>Hello{name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptatem debitis vitae sunt ab similique facilis perspiciatis ipsa corrupti consectetur rem, animi unde quis velit quo, hic tempore aperiam excepturi.</p>
-    </div>
-
+    <Navbar title="ReactApp"/>
     </>
   );
 }
